2e4225c7cadfd2b150aab5c6598fcda784aaa444
jinjiawu <jinjiawu@arimacomm.com.tw>
2021-04-07 17:31:16 +0800

    Add echo reference with QUIN_MI2S path.
    Root cause:
    VoIP performance.
    
    How to fix:
    Create a routing paths for echo reference.
    
    RiskArea: Audio
    Change-Id: I845998e27bc229f7853cdd4fcf6571a402e6427d

--- asoc/msm-pcm-routing-v2.c
+++ asoc/msm-pcm-routing-v2.c
@@ -3796,6 +3796,12 @@ static int msm_routing_ec_ref_rx_put(struct snd_kcontrol *kcontrol,
 		msm_route_ec_ref_rx = 24;
 		ec_ref_port_id = AFE_PORT_ID_PRIMARY_TDM_RX_1;
 		break;
+//[FairPhone][Audio][jinjia]=2021.04.07=Add QUIN_MI2S path for echo reference. -s
+	case 25:
+		msm_route_ec_ref_rx = 25;
+		ec_ref_port_id = AFE_PORT_ID_QUINARY_MI2S_RX;
+		break;
+//[FairPhone][Audio][jinjia]=2021.04.07=Add QUIN_MI2S path for echo reference. -e
 	default:
 		msm_route_ec_ref_rx = 0; /* NONE */
 		pr_err("%s EC ref rx %ld not valid\n",
@@ -3820,7 +3826,7 @@ static const char *const ec_ref_rx[] = { "None", "SLIM_RX", "I2S_RX",
 	"QUAT_TDM_RX_0", "QUAT_TDM_RX_1", "QUAT_TDM_RX_2", "SLIM_6_RX",
 	"TERT_MI2S_RX", "QUAT_MI2S_RX", "TERT_TDM_TX_0", "USB_AUDIO_RX",
 	"INT0_MI2S_RX", "INT4_MI2S_RX", "INT3_MI2S_TX", "DISPLAY_PORT",
-	"PRI_TDM_RX_1"};
+	"PRI_TDM_RX_1","QUIN_MI2S_RX"}; //[FairPhone][Audio][jinjia]=2021.04.07=Add QUIN_MI2S path for echo reference. - "PRI_TDM_RX_1"};
 
 static const struct soc_enum msm_route_ec_ref_rx_enum[] = {
 	SOC_ENUM_SINGLE_EXT(ARRAY_SIZE(ec_ref_rx), ec_ref_rx),
@@ -18012,6 +18018,7 @@ static const struct snd_soc_dapm_route intercon[] = {
 	{"AUDIO_REF_EC_UL1 MUX", "SEC_MI2S_TX", "SEC_MI2S_TX"},
 	{"AUDIO_REF_EC_UL1 MUX", "TERT_MI2S_TX", "TERT_MI2S_TX"},
 	{"AUDIO_REF_EC_UL1 MUX", "QUAT_MI2S_TX", "QUAT_MI2S_TX"},
+	{"AUDIO_REF_EC_UL1 MUX", "QUIN_MI2S_RX", "QUIN_MI2S_RX"},	//[FairPhone][Audio][jinjia]=2021.04.07=Add QUIN_MI2S path for echo reference.
 	{"AUDIO_REF_EC_UL1 MUX", "SLIM_1_TX", "SLIMBUS_1_TX"},
 	{"AUDIO_REF_EC_UL1 MUX", "PRI_TDM_TX_3", "PRI_TDM_TX_3"},
 	{"AUDIO_REF_EC_UL1 MUX", "QUAT_TDM_TX_1", "QUAT_TDM_TX_1"},
